3de423801fb56b46a2b4577c4badbbf2f20ad5fc
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-05-07 11:42:08 -0400

    Fully working script

--- Scripts/migrate-to-slices.py
+++ Scripts/migrate-to-slices.py
@@ -1,3 +1,4 @@
+import os
 import geopandas as gpd
 from datetime import date, datetime
 from typing import Dict, Any
@@ -25,7 +26,7 @@ missing_road_fields_mapping: Dict[str,str] = {
 rail_fields_mapping: Dict[str, str] = {
     date_field: 'First Seen',
 }
-years : list[int]= [1877,1900,1910]
+years : list[int]= [1880,1900,1910,1920]
 
 def pull_slice(source_shapefilefolder, destination_shapefilefolder, year, last_year_str):
   pull_features(f"{source_shapefilefolder}/Missing/"+"Missing Streets.shp",
@@ -33,7 +34,7 @@ def pull_slice(source_shapefilefolder, destination_shapefilefolder, year, last_y
                 destination_missing_roadname,
                 last_year_str
                 ,year,
-                missing_date_field,
+                date_field,
                 missing_road_fields_mapping, True)
   pull_features(f"{source_shapefilefolder}/Streets/streets.shp", 
                 destination_shapefilefolder,
@@ -45,17 +46,28 @@ def pull_slice(source_shapefilefolder, destination_shapefilefolder, year, last_y
   last_year = year
 # Define the field mapping from the source to the destination
     
-
+def convert_field_to_dt(date_field, source_gdf):
+    if date_field in source_gdf.columns:
+        try:
+            source_gdf[date_field] = gpd.pd.to_datetime(source_gdf[date_field], errors='coerce').dt.date
+            # Convert to datetime, handle errors, then extract only the date part
+        except Exception as e:
+            print(f"Error converting '{date_field}' to date: {e}")
+            exit()
+    else:
+        print(f"Error: '{date_field}' not found in the source shapefile.")
+        exit()
 
 
 
 def pull_features(source_shapefile, destination_shapefile_folder:str, destinationShapeFileName:str, last_year: int,
 year: int, date_field: str, 
 fields_mapping: Dict[str,str], missing: bool = False):
+    destination_dir = destination_shapefile_folder+f"/{year}"
     if last_year != '0000':
-        destinationShapeFileName = f"{destination_shapefilefolder}/{year}/{last_year}-{year} {destinationShapeFileName}"
+        destinationShapeFileName = f"{destination_dir}/{last_year}-{year} {destinationShapeFileName}"
     else :
-        destinationShapeFileName = f"{destination_shapefilefolder}/{year}/{year} {destinationShapeFileName}"
+        destinationShapeFileName = f"{destination_dir}/{year} {destinationShapeFileName}"
     try:
         source_gdf: gpd.GeoDataFrame = gpd.read_file(source_shapefile)
     except FileNotFoundError:
@@ -69,37 +81,25 @@ fields_mapping: Dict[str,str], missing: bool = False):
     print(f"loading time :{last_year}-{year} for {source_shapefile} to {destinationShapeFileName}")
     threshold_date: date = datetime.strptime(threshold_date_str, '%Y-%m-%d').date()
 
-# Ensure the date field is in datetime format
-    #if date_field in source_gdf.columns:
-    #    try:
-    #        source_gdf[date_field] = gpd.pd.to_datetime(source_gdf[date_field], errors='coerce')
-    #    # 'errors='coerce'' will turn invalid parsing into NaT (Not a Time)
-    #    except Exception as e:
-    #        print(f"Error converting '{date_field}' to datetime: {e}")
-    #        exit()
-    #else:
-    #    print(f"Error: '{date_field}' not found in the source shapefile.")
-    #    exit()
-
-    if date_field in source_gdf.columns:
-        try:
-            source_gdf[date_field] = gpd.pd.to_datetime(source_gdf[date_field], errors='coerce').dt.date
-            # Convert to datetime, handle errors, then extract only the date part
-        except Exception as e:
-            print(f"Error converting '{date_field}' to date: {e}")
-            exit()
-    else:
-        print(f"Error: '{date_field}' not found in the source shapefile.")
-        exit()
+    convert_field_to_dt(date_field, source_gdf)
+    if missing:
+        convert_field_to_dt(missing_date_field, source_gdf)
+    
 
 # Filter the source GeoDataFrame based on the date condition
     filtered_gdf: gpd.GeoDataFrame
     if last_year == "0000":
-        filtered_gdf = source_gdf[source_gdf[date_field] < threshold_date ].copy()
+        if missing :
+            filtered_gdf = source_gdf[(source_gdf[date_field] < threshold_date) & (source_gdf[missing_date_field].isna()) | (source_gdf[missing_date_field]>threshold_date) ].copy()
+        else:
+            filtered_gdf = source_gdf[source_gdf[date_field] < threshold_date].copy()
     else:
-        filtered_gdf = source_gdf[ (source_gdf[date_field] < threshold_date) & (source_gdf[date_field] > last_date) ].copy()
-    if missing:
-        filtered_gdf =filtered_gdf[filtered_gdf[missing_date_field]<threshold_date].copy()
+        if missing:
+            filtered_gdf = source_gdf[ (source_gdf[date_field] < threshold_date) & (source_gdf[date_field] > last_date) & (source_gdf[missing_date_field].isna()) | (source_gdf[missing_date_field]>threshold_date)].copy()
+        else :
+            filtered_gdf = source_gdf[ (source_gdf[date_field] < threshold_date) & (source_gdf[date_field] > last_date) ].copy()       
+    features = len(filtered_gdf)
+
 
 # Create a new GeoDataFrame for the destination with the mapped fields
     destination_data: Dict[str, Any] = {}
@@ -116,15 +116,22 @@ fields_mapping: Dict[str,str], missing: bool = False):
 
 # Save the new GeoDataFrame to the destination shapefile
     try:
+        if f"{destination_shapefile_folder}/{year}" and not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+            print(f"Created destination directory: {destination_dir}")
         destination_gdf.to_file(destinationShapeFileName)
         print(f"Successfully migrated {len(filtered_gdf)} features to {destinationShapeFileName}")
     except FileNotFoundError:
         print(f"Error: destination shapefile not found at {destinationShapeFileName}")
     except Exception as e:
         print(f"Error saving the destination shapefile: {e}")
+
+
 last_year = "0000"
 for year in years :
     pull_slice(source_shapefilefolder,destination_shapefilefolder,year, last_year)
     last_year = year 
 
     
+
+
