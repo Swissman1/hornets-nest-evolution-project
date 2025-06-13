af9ab4022a0e983828b6fd257e3b8af48fbdb6ea
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-06-13 09:34:25 -0400

    more slices

--- Scripts/feature_filter.py
+++ Scripts/feature_filter.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 import numpy as np
 import operator
 from functools import reduce
 from typing import Dict
+import pandas as pd
 
 # These should be imported or passed in, but for now, we keep them as module-level variables for compatibility
 missing_date_field = 'date Rem'
@@ -33,12 +35,19 @@ class FeatureFilter:
     def filter_features(last_year, date_field, missing, source_gdf, last_date, threshold_date, isRail=None):
         conditions = []
         conditions.append(source_gdf[date_field] < threshold_date)
+# Ensure the date_field is a datetime object
+        source_gdf[date_field] = pd.to_datetime(source_gdf[date_field])
         if last_year != "0000":
-            conditions.append(source_gdf[date_field] > last_date)
+            conditions.append(source_gdf[date_field].dt.date > last_date)
+                # New condition for 1999 and '12-01-1999'
+        if int(last_year) <= 1999 and threshold_date >= datetime(1999, 12, 31).date():
+            exclude_date_1999 = datetime(1999, 12, 30).date()
+            conditions.append(source_gdf[date_field].dt.date != exclude_date_1999)
         if missing:
             conditions.append(
                 (source_gdf[missing_date_field].isna()) | (source_gdf[missing_date_field] >= threshold_date)
             )
+        
         mask = reduce(operator.and_, conditions)
         filtered_gdf = source_gdf[mask].copy()
         return filtered_gdf
