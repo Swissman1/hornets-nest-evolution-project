bd5bc419b3f9cac80bf75d82e7924e85549fbd97
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-06-19 10:55:25 -0400

    added full

--- Scripts/migrate-to-slices.py
+++ Scripts/migrate-to-slices.py
@@ -4,7 +4,7 @@ from migration_manager import MigrationManager
 # Define the file paths and years
 source_shapefilefolder: str = '../Derived-Layers'
 destination_shapefilefolder: str = '../Derived-Layers/Time Slices'
-years = [1800,1860,1880,1900,1920,1950,1980,1995,2025]
+years = [2025]
 
 if __name__ == "__main__":
     processor = ShapefileProcessor(source_shapefilefolder, destination_shapefilefolder)
